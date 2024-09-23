6daa65016575e23c547773e75ccf3d82c90e764b
Angelino <angelino_m@outlook.com>
2024-09-23 12:00:22 +0200

    Day 005

--- win32_jinogame.cpp
+++ win32_jinogame.cpp
@@ -22,17 +22,35 @@ struct offscreen_buffer
     int Width;
     int Height;
     int Pitch;
-    int BytesPerPixel;
+};
+
+struct window_dimensions
+{
+    int Width;
+    int Height;
 };
 
 // TODO: These shouldn't be global forever
 global_variable bool Running;
 global_variable offscreen_buffer GlobalBackBuffer;
 
+internal_func window_dimensions
+J_GetWindowDimensions(HWND Window)
+{
+    window_dimensions Result = {};
+
+    RECT ClientRect;
+    GetClientRect(Window, &ClientRect);
+    Result.Width = ClientRect.right - ClientRect.left;
+    Result.Height = ClientRect.bottom - ClientRect.top;
+
+    return Result;
+}
+
 internal_func void
 RenderWeirdGradient(offscreen_buffer Buffer, int XOffset, int YOffset)
 {
-    // TODO: see what the optimizer does if we pass Buffer by value VS by pointer
+    // TODO: See what the optimizer does if we pass Buffer by value VS by pointer
     uint8* Row = (uint8*)Buffer.Memory;
     for (int Y = 0; Y < Buffer.Height; ++Y)
     {
@@ -41,10 +59,10 @@ RenderWeirdGradient(offscreen_buffer Buffer, int XOffset, int YOffset)
         {
             /*
                Pixel:           RR GG BB xx
-     Actually in memory it is:  BB GG RR xx
+     Actually in memory it is:  BB GG RR xx           <- Because Microsoft decided to do it that way
 
      And it's Little-Endian, so in the register it's:
-                                xx RR GG BB
+                                xx RR GG BB           <- Probably this is why they decided that. So in Little-Endian they can have order RGB
                                 ↑  ↑  ↑  ↑
 Padding : 0xFF << 8 * 3 sets FF ┘  |  |  |
     Red :    0xFF << 8 * 2 sets FF ┘  |  |
@@ -53,7 +71,7 @@ Padding : 0xFF << 8 * 3 sets FF ┘  |  |  |
             */
             uint8 Blue = X + XOffset;
             uint8 Green = Y + YOffset;
-            uint8 Red = 0;
+            uint8 Red = (XOffset + YOffset)/2;
             uint8 Padding = 0;
 
             *Pixel++ = ((Padding << 8*3) |
@@ -75,7 +93,7 @@ J_ResizeDIBSection(offscreen_buffer *Buffer, int Width, int Height)
     
     Buffer->Width = Width;
     Buffer->Height = Height;
-    Buffer->BytesPerPixel = 4;
+    int BytesPerPixel = 4;
     
     Buffer->Info.bmiHeader.biSize = sizeof(Buffer->Info.bmiHeader);
     Buffer->Info.bmiHeader.biWidth = Buffer->Width;
@@ -84,26 +102,23 @@ J_ResizeDIBSection(offscreen_buffer *Buffer, int Width, int Height)
     Buffer->Info.bmiHeader.biBitCount = 32; // TODO: Learn why we want to be DWORD-aligned (this is why 32 was chosen)
     Buffer->Info.bmiHeader.biCompression = BI_RGB;
 
-    int BitmapMemorySize = Buffer->BytesPerPixel * Buffer->Width * Buffer->Height;
+    int BitmapMemorySize = BytesPerPixel * Buffer->Width * Buffer->Height;
     Buffer->Memory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
     
-    Buffer->Pitch = Buffer->Width * Buffer->BytesPerPixel;
+    Buffer->Pitch = Buffer->Width * BytesPerPixel;
     
-    // TODO: Clear this to black
+    // TODO: Clear this to black?
 }
 
 internal_func void
-J_DisplayBufferInWindow(HDC DeviceContext, RECT ClientRect, 
-                        offscreen_buffer Buffer,
-                        int X, int Y, int Width, int Height)
+J_DisplayBufferInWindow(HDC DeviceContext, int WindowWidth, int WindowHeight, 
+                        offscreen_buffer Buffer)
 {
-    int WindowWidth = ClientRect.right - ClientRect.left;
-    int WindowHeight = ClientRect.bottom - ClientRect.top;
+    // TODO: Aspect ratio correction
+    // TODO: Play with stretch modes
     StretchDIBits(DeviceContext,
-                  /*X, Y, Width, Height,
-                  X, Y, Width, Height,*/
-                  0, 0, Buffer.Width, Buffer.Height,
                   0, 0, WindowWidth, WindowHeight,
+                  0, 0, Buffer.Width, Buffer.Height,
                   Buffer.Memory,
                   &(Buffer.Info),
                   DIB_RGB_COLORS,
@@ -120,11 +135,6 @@ LRESULT CALLBACK J_MainWindowCallback(HWND Window,
     {
     case WM_SIZE:
     {
-        RECT ClientRect;
-        GetClientRect(Window, &ClientRect);
-        int Width = ClientRect.right - ClientRect.left;
-        int Height = ClientRect.bottom - ClientRect.top;
-        J_ResizeDIBSection(&GlobalBackBuffer, Width, Height);
     } break;
 
     case WM_CLOSE:
@@ -146,23 +156,17 @@ LRESULT CALLBACK J_MainWindowCallback(HWND Window,
     {
         PAINTSTRUCT Paint;
         HDC DeviceContext = BeginPaint(Window, &Paint);
-        int X = Paint.rcPaint.left;
-        int Y = Paint.rcPaint.right;
         int Width = Paint.rcPaint.right - Paint.rcPaint.left;
         int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
 
-        RECT ClientRect;
-        GetClientRect(Window, &ClientRect);
-        J_DisplayBufferInWindow(DeviceContext, ClientRect,
-                                GlobalBackBuffer,
-                                X, Y, Width, Height);
+        J_DisplayBufferInWindow(DeviceContext, Width, Height,
+                                GlobalBackBuffer);
         EndPaint(Window, &Paint);
     } break;
 
     default:
     {
-        //OutputDebugString(L"default\n");
-        Result = DefWindowProc(Window, Message, WParam, LParam);
+        Result = DefWindowProc(Window, Message, WParam, LParam); // Let Windows handle with default behavior
     } break;
     }
     return Result;
@@ -175,11 +179,12 @@ WinMain(HINSTANCE Instance,
     int ShowCode)
 {
     WNDCLASS WindowClass = {};
-    WindowClass.style = CS_HREDRAW | CS_VREDRAW; // Flags to redraw whole window instead of painting only the new section when resizing
+    WindowClass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC; // Flags to redraw whole window instead of painting only the new section when resizing
     WindowClass.lpfnWndProc = J_MainWindowCallback;
     WindowClass.hInstance = Instance;
     //WindowClass.hIcon;
     WindowClass.lpszClassName = L"JinoGameWindowClass";
+    J_ResizeDIBSection(&GlobalBackBuffer, 1366, 768);
 
     if (RegisterClass(&WindowClass))
     {
@@ -197,6 +202,7 @@ WinMain(HINSTANCE Instance,
             0);
         if (Window)
         {
+            HDC DeviceContext = GetDC(Window);
             int XOffset = 0;
             int YOffset = 0;
 
@@ -215,18 +221,13 @@ WinMain(HINSTANCE Instance,
                 }
                 RenderWeirdGradient(GlobalBackBuffer, XOffset, YOffset);
 
-                HDC DeviceContext = GetDC(Window);
-                RECT ClientRect;
-                GetClientRect(Window, &ClientRect);
-                int WindowWidth = ClientRect.right - ClientRect.left;
-                int WindowHeight = ClientRect.bottom - ClientRect.top;
-                J_DisplayBufferInWindow(DeviceContext, ClientRect, 
-                                        GlobalBackBuffer, 
-                                        0, 0, WindowWidth, WindowHeight);
-                ReleaseDC(Window, DeviceContext);
-
+                window_dimensions Dimensions = J_GetWindowDimensions(Window);
+                J_DisplayBufferInWindow(DeviceContext, Dimensions.Width, Dimensions.Height, 
+                                        GlobalBackBuffer);
                 ++XOffset;
+                YOffset += 2;
             }
+            ReleaseDC(Window, DeviceContext);
         }
         else
         {
